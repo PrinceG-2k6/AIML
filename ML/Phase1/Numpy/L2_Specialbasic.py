bf08623210dc4ebf1b04c217684a1c4964da86aa
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-09 12:59:58 +0530

    .

---
+++ ML/Phase1/Numpy/L2_Specialbasic.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# Zeroes
+# ar_zero =np.zeros(4)
+# ar_zero =np.zeros((3,4))
+
+# print(ar_zero)
+
+# ar_ones=np.ones(4)
+# ar_ones =np.ones((3,4))
+# print(ar_ones)
+
+
+# EMPTY
+
+# ar_empt= np.empty(8)
+# print(ar_empt)
+
+# RANGE
+# ar_rn = np.arange(4)
+# print(ar_rn)
+
+# DIAGONAL
+# ar_dia = np.eye(3)
+# # ar_dia = np.eye(3,5)
+# print(ar_dia)
+
+# LINESPACE
+ar_ln= np.linspace(0,100,num=5)
+print(ar_ln)
