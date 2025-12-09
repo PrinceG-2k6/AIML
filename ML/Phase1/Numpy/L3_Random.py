23c8589fb2e28cb4524c9f2df558d189b5ac1229
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-09 13:06:27 +0530

    .

---
+++ ML/Phase1/Numpy/L3_Random.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# rand()  between 0 to 1
+# var = np.random.rand(5)
+# var = np.random.rand(2,5)
+
+# randn() close to 0 (+ive and -ve)
+# var = np.random.randn(5)
+
+# ranf() 0 to 1 excluding 1.0
+# var = np.random.ranf(5)
+
+# randint() by given range
+var = np.random.randint(5,10,10)
+
+
+print(var)
