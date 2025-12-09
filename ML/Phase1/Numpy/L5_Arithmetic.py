40a6867287aae5434b84d5e4e315a5e887798fa2
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-09 13:27:46 +0530

    .

---
+++ ML/Phase1/Numpy/L5_Arithmetic.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+# # a+b np.add(a+b)
+# var = np.array([1,2,3,4])
+# # varadd=var +3
+# varadd= np.add(var,[1,2,3,4])
+
+# print(varadd)
+
+# # a-b np.subtract(a-b)
+# var = np.array([1,2,3,4])
+# varadd=var -3
+
+# varadd= np.subtract(var,[1,2,3,4])
+
+# print(varadd)
+
+# # a*b np.multiply(a*b)
+# var = np.array([1,2,3,4])
+# varadd=var *3
+
+# varadd= np.multiply(var,[1,2,3,4])
+
+# print(varadd)
+
+# # a/b np.divide(a/b)
+# var = np.array([1,2,3,4])
+# varadd=var /3
+
+# print(varadd)
+
+# # a%b np.mod(a%b)
+# var = np.array([1,2,3,4])
+# varadd=var %3
+
+# print(varadd)
+
+# # a**b np.power(a**b)
+# var = np.array([1,2,3,4])
+# varadd=var **3
+
+# print(varadd)
+
+# # 1/a np.reciprocal(a)
+# var = np.array([1,2,3,4])
+# varadd=np.reciprocal(var)
+
+# print(varadd)
+
+# same for 2D and 3D array
