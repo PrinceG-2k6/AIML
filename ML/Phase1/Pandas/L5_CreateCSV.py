0d012bdf7183b5c8bace643c72a0235171ef4218
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-10 23:07:18 +0530

    .

---
+++ ML/Phase1/Pandas/L5_CreateCSV.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+dis={
+    "a":[1,2,3,4,5,6],
+    "s":[2,3,4,5,6,7],
+    "d":[3,4,5,6,7,8]
+}
+d = pd.DataFrame(dis)
+
+print(d)
+# d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV.csv")
+# d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV1.csv",index=False)
+# d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",index=False,header=["x","y","z"])
