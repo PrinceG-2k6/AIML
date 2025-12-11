f9c5ffe56194e2c76160d0eb82b61ed45d75466c
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-11 17:13:45 +0530

    .

--- ML/Phase1/Pandas/L6_ReadCSV.py
+++ ML/Phase1/Pandas/L6_ReadCSV.py
@@ -15,10 +15,24 @@ print()
 # print()
 
 # csv_1= pd.read_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",index_col="x")
-# print(csv_1)
+# print(csv_1) 
 # print()
 
 
-csv_1= pd.read_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",header=1)
+# csv_1= pd.read_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",header=1)
+# print(csv_1)
+# print() 
+
+# csv_1= pd.read_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",names=["col1","col2","col3"])
+# print(csv_1)
+# print() 
+
+# OR
+
+# csv_1= pd.read_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",header=None,prefix="col")
+# print(csv_1)
+# print() 
+
+csv_1= pd.read_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",dtype={"x":"float","y":"string"})
 print(csv_1)
 print() 
