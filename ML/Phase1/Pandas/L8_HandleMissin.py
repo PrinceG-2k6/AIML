82bdea425a68bc3bc7930409f104f5f9f435a45e
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-11 17:50:17 +0530

    .

--- ML/Phase1/Pandas/L8_HandleMissin.py
+++ ML/Phase1/Pandas/L8_HandleMissin.py
@@ -1,1 +1,31 @@
 import pandas as pd 
+
+csv_1= pd.read_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv")
+print(csv_1)
+print() 
+
+# print(csv_1.dropna())
+
+# print(csv_1.dropna(axis=1))
+
+# print(csv_1.dropna(how="any"))
+# print(csv_1.dropna(how="all"))
+
+# print(csv_1.dropna(subset=["y"]))
+
+# print(csv_1.dropna(inplace=True))
+
+
+# print(csv_1.dropna(thresh=2))
+
+
+print(csv_1.fillna("Python",limit=1))
+
+# print(csv_1.fillna({"y":"Python","z":"Pyth"}))
+
+
+# print(csv_1.fillna(method="ffill"))
+# print(csv_1.fillna(method="ffill",axis=1))
+
+# csv_1.fillna(12,inplace=True)
+# print(csv_1)
