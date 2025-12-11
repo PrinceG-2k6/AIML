839526bfa790a42f25c5b7255cf4c8511e53ec9c
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-11 17:38:00 +0530

    .

--- ML/Phase1/Pandas/L5_CreateCSV.py
+++ ML/Phase1/Pandas/L5_CreateCSV.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 dis={
     "a":[1,2,3,4,5,6],
-    "s":[2,3,4,5,6,7],
-    "d":[3,4,5,6,7,8]
+    "s":[2,3,4,5,6,None],
+    "d":[3,4,5,6,None,None]
+
 }
 d = pd.DataFrame(dis)
 
 print(d)
-# d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV.csv")
-# d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV1.csv",index=False)
-# d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",index=False,header=["x","y","z"]) 
+d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV.csv")
+d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV1.csv",index=False)
+d.to_csv("./ML/Phase1/Pandas/L5_CreateCSV2.csv",index=False,header=["x","y","z"]) 
